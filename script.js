c4d8c54cd59f5b6b06efac5a8ed3ffca145b5b8c
Willia Developer <circuitbreak2020@gmail.com>
2025-01-07 09:48:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -144,3 +144,16 @@ taskInput.addEventListener("keypress", (e) => {
 
 // Renderizar Tarefas ao Carregar
 renderTasks();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado com sucesso:", registration);
+      })
+      .catch((error) => {
+        console.log("Falha ao registrar o Service Worker:", error);
+      });
+  });
+}
